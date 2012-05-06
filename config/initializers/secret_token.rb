b57392ea0fff6ef4c7a377f0cf4b57bc859fd6e6
vlad-len <vlad-len@bigmir.net>
2012-05-06 18:59:50 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catalog::Application.config.secret_token = '67ef0fdc7ced6e61c2557145bc884aecc03d98c4d5c4c5d200a312972989b0db3f7184ba68afdd1c877173f460b49186ca4d19b97cd7309ae5cc54c39f81dcad'
---
+++ db/migrate/20120506155452_create_offers.rb
@@ -0,0 +1,17 @@
+class CreateOffers < ActiveRecord::Migration
+  def change
+    create_table :offers do |t|
+      t.string :title
+      t.text :description
+      t.string :short_description
+      t.string :phone1
+      t.string :phone2
+      t.string :email
+      t.string :city
+      t.string :address
+      t.integer :cat_id
+
+      t.timestamps
+    end
+  end
+end
