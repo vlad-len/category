d93063c31ea8a8a8c044caa97689587dc0ae874b
vlad-len <vlad-len@bigmir.net>
2012-05-06 20:41:43 +0300

    trololo

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,29 @@
 class CatsController < ApplicationController
+  def  index
+   @cats=Cat.all
+  end
+  def new
+    @cats=Cat.new
+  end
+  def create
+    @cats=Cat.new(params[:cat])
+    @cats.save
+    redirect_to cat_path(@cats)
+  end
+  def edit
+    @cats=Cat.find(params[:id])
+  end
+  def show
+    @cats=Cat.find(params[:id])
+  end
+  def update
+    @cats=Cat.find(params[:id])
+    @cats.update_attributes(params[:cat])
+    redirect_to cat_path(@cats)
+  end
+  def destroy
+    @cats=Cat.find(params[:id])
+    @cats.destroy 
+    redirect_to cats_path(@cats)
+  end
 end
