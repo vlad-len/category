180e06a20b90faca8043246246520896030bb539
vlad-len <vlad-len@bigmir.net>
2012-08-14 22:49:02 +0300

    fixed posts on main

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,19 +2,11 @@ class PostsController < ApplicationController
   def index
     @category=Category.find(params[:category_id])
     @posts=Post.all
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post=Post.new(params[:post])
-    @post.save
-    redirect_to categories_path
+    @categories=Category.all
   end
 
   def show
     @post=Post.find(params[:id])
+    @categories=Category.all
   end
 end
